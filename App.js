596d46ccb7e597e71edd0a4faf8c07ad8baafe2f
zigang93 <tanzigang93@gmail.com>
2019-03-08 10:49:00 +0800

    add loading icon and images

--- App.js
+++ App.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+
 import Avatar from './components/Avatar';
 import { Constants } from 'expo';
 import AuthorRow from './components/AuthorRow';
-
+import Card from './components/Card';
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <AuthorRow
-          fullname={'Zi Gang'}
+        <Card
+          fullname={'First Last'}
           linkText={'Comments'}
           onPressLinkText={() => {
             console.log('Pressed link!');
           }}
+          image={{ uri: 'https://unsplash.it/600/600' }}
         />
       </View>
     );
---
+++ components/Card.js
@@ -0,0 +1,66 @@
+import { ActivityIndicator, Image, StyleSheet, View } from 'react-native';
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+
+import AuthorRow from './AuthorRow';
+
+class Card extends Component {
+
+  static PropTypes ={
+    fullname: PropTypes.string.isRequired,
+    image: Image.propTypes.source.isRequired,
+    linkText: PropTypes.string,
+    onPressLinkText: PropTypes.func,
+  }
+
+  static defaultProps = { 
+    linkText: '', 
+    onPressLinkText: () => {},
+  };
+
+  state = { 
+    loading: true,
+  }
+
+  handleLoad = () => { 
+    this.setState({ 
+      loading: false 
+    });
+  };
+
+  render() {
+
+    const { fullname, image, linkText, onPressLinkText } = this.props;
+    const { loading } = this.state;
+
+    return (
+      <View>
+        <AuthorRow
+          fullname={fullname}
+          linkText={linkText}
+          onPressLinkText={onPressLinkText}
+        />
+        <View style={styles.image}>
+          {loading && (
+            <ActivityIndicator style={StyleSheet.absoluteFill} size={'large'} />
+          )}
+          <Image 
+            style={StyleSheet.absoluteFill} 
+            source={image} 
+            onLoad={this.handleLoad}
+          />
+
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({ 
+  image: {
+    aspectRatio: 1,
+    backgroundColor: 'rgba(0,0,0,0.02)',
+  },
+});
+
+export default Card;
